3a366b36fd58ac5475c012ea63f1089548410ca5
Christian <mail@christiansievers.info>
2021-03-14 19:17:18 +0100

    first version
    first version, many unused tile maps, to clean up, need to credit map creators b4 publishing

---
+++ opengameart.org:content:lpc-streets/road_markings_white.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="road_markings_white" tilewidth="32" tileheight="32" tilecount="225" columns="15">
+ <image source="road_markings_white.png" width="480" height="480"/>
+</tileset>
---
+++ opengameart.org:content:lpc-streets/street_light.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="street_light" tilewidth="32" tileheight="32" tilecount="24" columns="6">
+ <image source="street_light.png" width="192" height="128"/>
+</tileset>
