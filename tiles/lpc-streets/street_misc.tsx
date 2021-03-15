39093ae587b25b135ee20bc4f7f2ffe44c868d74
christiansievers <mail@christiansievers.info>
2021-03-15 13:46:39 +0000

    Deploying to gh-pages from master @ 54fc0a267f147238f445b51efec6f4111819811d 🚀

--- tiles/lpc-streets/street_misc.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="street_misc" tilewidth="32" tileheight="32" tilecount="14" columns="2">
- <image source="street_misc.png" width="64" height="224"/>
-</tileset>
